d0d37b186e74367889b2458cc1fffc63bcbec3f3
Luke Alkema <lalkema0848@conestogac.on.ca>
2024-01-10 13:37:34 -0500

    Added functions needed

---
+++ assessGrade.cpp
@@ -0,0 +1,22 @@
+
+void assessGrade(char* letter)
+{
+
+}
+
+void assessGrade(double finalMark)
+{
+
+}
+
+
+void assessGrade(int assignmentMarks[])
+{
+
+}
+
+
+int parseUserInput(char buffer[])
+{
+
+}
